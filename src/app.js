60780468a132898d7798152a416d92c9b7a25e44
Khayyam Jones <Khayyam.Jones@gmail.com>
2017-06-05 17:05:00 -0600

    created full board

--- src/app.js
+++ src/app.js
@@ -1,14 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-// import Knight from './Knight/knight'
-// import Square from './Knight/square'
 import Board from './Knight/board'
 import './Knight/Knight.scss'
 
 ReactDOM.render(
-  // <Square black>
-  //   <Knight />
-  // </Square>,
-  <Board knightPosition={[0, 0]} />,
+  <Board knightPosition={[7, 4]} />,
   document.getElementById('root')
 )
