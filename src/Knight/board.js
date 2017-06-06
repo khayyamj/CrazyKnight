df7f4a44111a5e2209ca79ca85a1fdfcc74eb123
Khayyam Jones <Khayyam.Jones@gmail.com>
2017-06-05 18:53:53 -0600

    knight is now draggable and droppable

--- src/Knight/board.js
+++ src/Knight/board.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react'
 import PropTypes from 'prop-types'
-import Square from './square'
+import BoardSquare from './boardSquare'
 import Knight from './knight'
 import { canMoveKnight, moveKnight } from './game'
 import { DragDropContext } from 'react-dnd'
@@ -10,23 +10,22 @@ class Board extends Component {
   renderSquare (i) {
     const x = i % 8
     const y = Math.floor(i / 8)
-    const black = (x + y) % 2 === 1
-
-    const [knightX, knightY] = this.props.knightPosition
-    const piece = (x === knightX && y === knightY)
-      ? <Knight />
-      : null
-
     return (
-      <div
-        key={i}
-        style={{ width: '12.5%', height: '12.5%' }}
-        onClick={() => this.handleSquareClick(x, y)}>
-        <Square black={black}>
-          {piece}
-        </Square>
+      <div key={i}
+        style={{ width: '12.5%', height: '12.5%' }}>
+        <BoardSquare x={x}
+          y={y}>
+          {this.renderPiece(x, y)}
+        </BoardSquare>
       </div>
     )
+  }
+
+  renderPiece (x, y) {
+    const [knightX, knightY] = this.props.knightPosition
+    if (x === knightX && y === knightY) {
+      return <Knight />
+    }
   }
 
   handleSquareClick (toX, toY) {
